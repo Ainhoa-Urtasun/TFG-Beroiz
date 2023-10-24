505b7bb64d6f91f349f3d49ff8f24d08fd18d77e
Ainhoa <ainhoa.urtasun@unavarra.es>
2023-10-24 18:28:15 +0200

    Update TFG-Beroiz.py

--- TFG-Beroiz.py
+++ TFG-Beroiz.py
@@ -20,17 +20,22 @@ data = data.reindex(range(0,n),fill_value=0)
 structure = [pandas.DataFrame({key:val for key,val in metadata['dimension'][dim]['category'].items()}).sort_values('index')['label'].values for dim in metadata['id']]
 data.index = pandas.MultiIndex.from_product(structure,names=metadata['id'])
 mydata = data.reset_index()
-mydata = mydata[mydata.time=='2022']
 mydata = mydata[mydata['prof_pos']=='Executives']
 mydata = mydata[['geo',0]]
 mydata.rename(columns={0:'Percentage'},inplace=True)
 mydata.rename(columns={'geo':'ADMIN'},inplace=True)
 
-world = geopandas.read_file('/content/PRACTICES/ne_110m_admin_0_countries.zip')[['ADMIN','geometry']]
+mydata1 = mydata[mydata.time=='2022']
+table = mydata.pivot(index='ADMIN',columns='time',values=['percentage']).reset_index()
+table.columns = table.columns.droplevel(level=0)
+table.rename(columns={'ADMIN':'GEO'},inplace=True)
+table = table.rename_axis(columns=None)
+
+world = geopandas.read_file('/content/TFG-Beroiz/ne_110m_admin_0_countries.zip')[['ADMIN','geometry']]
 polygon = Polygon([(-25,35),(40,35),(40,75),(-25,75)])
 europe = geopandas.clip(world,polygon)
 
-mydata = mydata.merge(europe,on='ADMIN',how='right')
+mydata1 = mydata1.merge(europe,on='ADMIN',how='right')
 mydata = geopandas.GeoDataFrame(mydata,geometry='geometry')
 fig,ax = plt.subplots(1,figsize=(10,10))
 mydata.plot(column='Percentage',alpha=0.8,cmap='viridis',ax=ax,legend=True)
